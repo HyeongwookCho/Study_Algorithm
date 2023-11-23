e36a578a9e71484f3ece64db3d12ec222719a614
HyeongwookCho <86861803+HyeongwookCho@users.noreply.github.com>
2023-11-23 11:28:35 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -5,11 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            sum += i;
-        }
-        System.out.println(sum);
+        System.out.println(n*(n+1)/2);
     }
 }
