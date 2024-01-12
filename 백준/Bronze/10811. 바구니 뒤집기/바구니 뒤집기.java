2a59a623c366ccf6b332bae66eebe987f913e767
HyeongwookCho <86861803+HyeongwookCho@users.noreply.github.com>
2024-01-12 15:01:19 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 144 ms, Memory: 15964 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,33 +1,46 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        String[] firstLine = br.readLine().split(" ");
+        int N = Integer.parseInt(firstLine[0]);
+        int M = Integer.parseInt(firstLine[1]);
 
-        int i = 0;
-        int j = 0;
-        int temp = 0;
-
-        int[] arr = new int[N];
-        for (int sep = 0; sep < arr.length; sep++) {
-            arr[sep] = sep + 1;
+        int[] baskets = new int[N];
+        for (int k = 0; k < N; k++) {
+            baskets[k] = k + 1;
         }
 
-        for (int rep = 0; rep < M; rep++) {
-            i = sc.nextInt()-1;
-            j = sc.nextInt()-1;
-
-            for (; i < j; i++, j--) {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+        for (int m = 0; m < M; m++) {
+            String[] range = br.readLine().split(" ");
+            int i = Integer.parseInt(range[0]) - 1;
+            int j = Integer.parseInt(range[1]) - 1;
+            reverseArray(baskets, i, j);
         }
-        for (int q = 0; q < arr.length; q++) {
-            System.out.print(arr[q] + " ");
+
+        for (int num : baskets) {
+            bw.write(num + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void reverseArray(int[] array, int start, int end) {
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
         }
     }
 }
