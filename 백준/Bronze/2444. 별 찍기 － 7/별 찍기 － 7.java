30fbc6cf7db461f469211bbb20a1e23c4fb8afca
HyeongwookCho <86861803+HyeongwookCho@users.noreply.github.com>
2024-01-25 16:49:01 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -3,18 +3,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         br.close();
+
         for (int i = 1; i <= N; i++) {
-            System.out.print(" ".repeat(N - i));
-            System.out.print("*".repeat(i*2-1));
-            System.out.print("\n");
+            bw.write(" ".repeat(N - i));
+            bw.write("*".repeat(i * 2 - 1));
+            bw.write("\n");
         }
         for (int j = N - 1; j > 0; j--) {
-            System.out.print(" ".repeat(N - j));
-            System.out.print("*".repeat(j*2-1));
-            System.out.print("\n");
+            bw.write(" ".repeat(N - j));
+            bw.write("*".repeat(j * 2 - 1));
+            bw.write("\n");
         }
+
+        bw.flush();
+        bw.close();
     }
 }
