aa9f80619b5598b35b10447e2ce117df81cfb6e9
HyeongwookCho <86861803+HyeongwookCho@users.noreply.github.com>
2024-02-04 15:11:14 +0900

    [Silver V] Title: 분수찾기, Time: 148 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -10,12 +10,12 @@ public class Main {
         // 해당 범위의 대각선 칸 개수
         int cross_count = 1;
         // 해당 대각선 직전 대각선까지의 칸의 누적 합
-        int prev_count_sum = 0;
+        int prev_count_sum = 0; // ex) 3번째 대각선 줄이면 prev_count_sum = 4 임 => 3/1, 2/2, 1.3 들의 분자 분모 합
 
         while (true) {
-            // 직전 대각선 노적 합 + 해당 대각선 개수 이요한 범위 판별
+            // 직전 대각선 누적 합 + 해당 대각선 개수 이용한 범위 판별
             if (X <= prev_count_sum + cross_count) {
-                if(cross_count % 2 == 1){ // 대각선의 개수가 짝수라면
+                if(cross_count % 2 == 1){ // 대각선의 개수가 홀수라면
                     // 분자가 큰 수부터 시작
                     // 분자는 대각선상 내의 블럭 개수 - (X 번째 - 직전 대각선까지의 블럭 개수 - 1)
                     // 분모는 X 번째 - 직전 대각선까지의 블럭 개수
