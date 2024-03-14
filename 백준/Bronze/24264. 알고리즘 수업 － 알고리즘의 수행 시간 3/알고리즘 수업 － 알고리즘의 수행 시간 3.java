7efb3562bbcd3acecfd155896d6ea7357ea3a2f7
HyeongwookCho <86861803+HyeongwookCho@users.noreply.github.com>
2024-03-14 20:17:19 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long a = Long.parseLong(br.readLine());
+
+        bw.write(Long.toString((long) Math.pow(a,2))+"\n");
+        bw.write(Integer.toString(2));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+
+}
