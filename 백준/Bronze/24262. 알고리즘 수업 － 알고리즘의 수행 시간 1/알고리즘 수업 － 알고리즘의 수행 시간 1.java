7ec8ea570e7868727e4634b605ff7643d3a07d15
HyeongwookCho <86861803+HyeongwookCho@users.noreply.github.com>
2024-03-14 20:05:33 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+
+        bw.write("1\n");
+        bw.write("0");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+
+}
